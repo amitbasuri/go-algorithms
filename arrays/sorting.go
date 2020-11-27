7a26f58424eda2b25789fc9e56d1287491744597
Amit Basuri <amitbasuri123@gmail.com>
2020-11-27 16:29:34 +0530

    added InsertionSort and SelectionSort

--- arrays/sorting.go
+++ arrays/sorting.go
@@ -17,3 +17,28 @@ func BubbleSort(array []int) []int {
 
 	return array
 }
+
+func InsertionSort(arr []int) []int {
+
+	for i := range arr {
+		for j := i; j > 0 && arr[j] < arr[j-1]; j-- {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
+		}
+	}
+	return arr
+}
+
+func SelectionSort(array []int) []int {
+	i := 0
+	for i < len(array)-1 {
+		min := i
+		for j := i + 1; j < len(array); j++ {
+			if array[j] < array[min] {
+				min = j
+			}
+		}
+		array[i], array[min] = array[min], array[i]
+		i++
+	}
+	return array
+}
